20ce70fc8f8c77d5b3d35d00d67de97ae9ca1005
EmreCandan0 <115104572+EmreCandan0@users.noreply.github.com>
2025-06-23 17:13:04 +0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,6 +1,5 @@
 from db import db_connection
-from osgeo import gdal,osr
-import os
+from osgeo import gdal, osr
 
 def get_epsg_from_dataset(dataset):
     proj = dataset.GetProjection()
@@ -8,35 +7,30 @@ def get_epsg_from_dataset(dataset):
     srs.ImportFromWkt(proj)
 
     if srs.IsProjected():
-        return srs.GetAttrValue("AUTHORITY", 1)  # EPSG kodu
+        return srs.GetAttrValue("AUTHORITY", 1)
     elif srs.IsGeographic():
-        return srs.GetAttrValue("AUTHORITY", 1)  # EPSG kodu
+        return srs.GetAttrValue("AUTHORITY", 1)
     else:
         return None
 
-def save_metadata(filename,upload_time,epsg,value,geom_str,dataset):
-    conn= db_connection()
-    cur=conn.cursor()
-    epsg_code = get_epsg_from_dataset(dataset)
-
+def save_metadata(filename, upload_time, epsg_code, value, geom_str, dataset):
+    conn = db_connection()
+    cur = conn.cursor()
     try:
         save = f"""
-        INSERT INTO tiff_metadata (filename,upload_time,epsg,value,geom)
-        VALUES (%s, %s, %s,%s, ST_Transform(ST_SetSRID(ST_GeomFromText(%s), {epsg_code}), 4326))
+        INSERT INTO tiff_metadata (filename, upload_time, epsg, value, geom)
+        VALUES (%s, %s, %s, %s, ST_Transform(ST_SetSRID(ST_GeomFromText(%s), {epsg_code}), 4326))
         """
-
-        cur.execute(save,(filename,upload_time,int(epsg_code),value,geom_str))
+        cur.execute(save, (filename, upload_time, int(epsg_code), value, geom_str))
         conn.commit()
-    except:
-        print("Metadata Insert Error")
+    except Exception as e:
+        print("Metadata Insert Error:", e)
     finally:
         cur.close()
         conn.close()
 
-
 def get_geom_wkt_and_bounds(dataset):
     gt = dataset.GetGeoTransform()
-
     minx = gt[0]
     maxy = gt[3]
     maxx = minx + (dataset.RasterXSize * gt[1])
@@ -44,30 +38,3 @@ def get_geom_wkt_and_bounds(dataset):
 
     polygon_wkt = f"POLYGON(({minx} {miny}, {minx} {maxy}, {maxx} {maxy}, {maxx} {miny}, {minx} {miny}))"
     return polygon_wkt, (minx, miny, maxx, maxy)
-
-
-def get_bounds_epsg4326(filepath):
-    gdal.UseExceptions()
-
-    # Projedeki tiffcropper/temp klasörünü kullan
-    reprojected_path = os.path.join("temp", "reproj_temp.tif")
-
-    dataset = gdal.Open(filepath)
-    if dataset is None:
-        raise Exception(f"Cannot open original file: {filepath}")
-
-    warp_result = gdal.Warp(reprojected_path, dataset, dstSRS='EPSG:4326')
-    if warp_result is None:
-        raise Exception(f"GDAL Warp failed for file: {filepath}")
-
-    ds = gdal.Open(reprojected_path)
-    if ds is None:
-        raise Exception(f"Failed to open reprojected file: {reprojected_path}")
-
-    gt = ds.GetGeoTransform()
-    minx = gt[0]
-    maxy = gt[3]
-    maxx = minx + (ds.RasterXSize * gt[1])
-    miny = maxy + (ds.RasterYSize * gt[5])
-
-    return minx, miny, maxx, maxy
